89d88a8156b05b60c53d698da27905e8581de4bb
MrSterdy <mrsterdy@gmail.com>
2023-09-28 09:51:33 +0300

    Fix subject service

--- src/lib/server/services/subjectService.ts
+++ src/lib/server/services/subjectService.ts
@@ -1,28 +1,14 @@
 import prisma from "$lib/server/db/prisma";
 import type { Subject } from "$lib/types";
 
-export async function getSubjects(): Promise<Subject[]> {
+export async function getSubjects(groupId: number): Promise<Subject[]> {
     const result = await prisma.subject.findMany();
 
     return result.map(subject => ({
-        id: subject.id,
         name: subject.name,
         teacher: subject.teacher,
         classroom: subject.classroom
     }));
-}
-
-export async function getSubjectById(id: number) {
-    const result = await prisma.subject.findFirst({ where: { id } });
-
-    return result
-        ? ({
-              id: result.id,
-              name: result.name,
-              teacher: result.teacher,
-              classroom: result.classroom
-          } as Subject)
-        : null;
 }
 
 export async function updateSubjects(groupId: number, subjects: Subject[]) {
--- src/routes/dashboard/group/[groupId]/subjects/+layout.server.ts
+++ src/routes/dashboard/group/[groupId]/subjects/+layout.server.ts
@@ -1,8 +1,8 @@
 import type { LayoutServerLoad } from "./$types";
 import { getSubjects } from "$lib/server/services/subjectService";
 
-export const load: LayoutServerLoad = async () => {
-    const subjects = await getSubjects();
+export const load: LayoutServerLoad = async event => {
+    const subjects = await getSubjects(parseInt(event.params.groupId));
 
     return { subjects };
 };
