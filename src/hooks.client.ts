3126ebb41a089a5d31201b3f06af70404638b2e9
MrSterdy <mrsterdy@gmail.com>
2023-10-04 10:04:40 +0300

    Implement back button

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,6 +2,8 @@ import dayjs from "dayjs";
 import "dayjs/locale/ru";
 
 window.Telegram.WebApp.ready();
+
+window.Telegram.WebApp.BackButton.onClick(() => window.history.back());
 window.Telegram.WebApp.BackButton.show();
 
 dayjs.locale("ru");
