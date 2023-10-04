5a2af3a5ef3f83b0761ea577ec9261e15cf238f9
MrSterdy <mrsterdy@gmail.com>
2023-10-04 20:18:46 +0300

    Expand web app after initialization, hide back button on main page

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -4,6 +4,7 @@ import "dayjs/locale/ru";
 window.Telegram.WebApp.ready();
 
 window.Telegram.WebApp.BackButton.onClick(() => window.history.back());
-window.Telegram.WebApp.BackButton.show();
+
+window.Telegram.WebApp.expand();
 
 dayjs.locale("ru");
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+    import { page } from "$app/stores";
+    import { browser } from "$app/environment";
+
     import "../app.css";
+
+    if (browser && $page.url.pathname === "/") {
+        window.Telegram.WebApp.BackButton.hide();
+    } else if (browser) {
+        window.Telegram.WebApp.BackButton.show();
+    }
 </script>
 
 <section class="prose">
