2ea03f3e7c3479be48bd2046135c1fb71aa52e85
MrSterdy <mrsterdy@gmail.com>
2023-10-04 19:04:18 +0300

    Add weekday timetables to main timetable page

--- src/routes/dashboard/group/[groupId]/timetable/+page.svelte
+++ src/routes/dashboard/group/[groupId]/timetable/+page.svelte
@@ -11,4 +11,28 @@
 
 <input type="date" id="date-select" value={formatISOString(new Date)} />
 
+<ul>
+    <li>
+        <a href="weekday/0">Воскресенье</a>
+    </li>
+    <li>
+        <a href="weekday/1">Понедельник</a>
+    </li>
+    <li>
+        <a href="weekday/2">Вторник</a>
+    </li>
+    <li>
+        <a href="weekday/3">Среда</a>
+    </li>
+    <li>
+        <a href="weekday/4">Четверг</a>
+    </li>
+    <li>
+        <a href="weekday/5">Пятница</a>
+    </li>
+    <li>
+        <a href="weekday/6">Cуббота</a>
+    </li>
+</ul>
+
 <button on:click={getTimetable}>Узнать расписание</button>
