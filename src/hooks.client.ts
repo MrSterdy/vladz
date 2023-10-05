0ba5cf3fbc7fd92035583180b4773a1b867d7661
MrSterdy <mrsterdy@gmail.com>
2023-10-05 08:49:08 +0300

    Add error handling to client hooks

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,12 @@
 import dayjs from "dayjs";
 import "dayjs/locale/ru";
+import type { HandleClientError } from "@sveltejs/kit";
+
+export const handleError: HandleClientError = ({ error }) => {
+    console.error(error);
+
+    return { message: "Произошла непредвиденная ошибка" };
+};
 
 window.Telegram.WebApp.ready();
 
