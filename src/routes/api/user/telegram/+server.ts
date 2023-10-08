90b8e8f7da04d027a6b9948993e9f5f979ae6592
MrSterdy <mrsterdy@gmail.com>
2023-10-08 16:23:36 +0300

    Skip authorization in dev mode

--- src/routes/api/user/telegram/+server.ts
+++ src/routes/api/user/telegram/+server.ts
@@ -5,16 +5,30 @@ import {
     generateJwt
 } from "$lib/server/services/telegramService";
 import { AUTH_TELEGRAM_COOKIE_NAME } from "$lib/consts";
+import type { TelegramUser } from "$lib/types";
+import { ADMIN_ID } from "$env/static/private";
 
 export async function POST(event: RequestEvent) {
-    const rawInitData = (await event.request.json()) as {
-        initData?: string;
-    } | null;
-    if (!rawInitData || !rawInitData.initData) {
-        throw error(400, { message: "Invalid initData" });
+    let telegramInfo: TelegramUser | null = null;
+
+    if (import.meta.env.DEV) {
+        telegramInfo = {
+            id: BigInt(ADMIN_ID),
+            first_name: "Влад",
+            last_name: "Король",
+            username: "vlad"
+        };
+    } else {
+        const rawInitData = (await event.request.json()) as {
+            initData?: string;
+        } | null;
+        if (!rawInitData || !rawInitData.initData) {
+            throw error(400, { message: "Invalid initData" });
+        }
+
+        telegramInfo = decodeInitData(rawInitData.initData);
     }
 
-    const telegramInfo = decodeInitData(rawInitData.initData);
     if (!telegramInfo) {
         throw error(400, { message: "Invalid initData" });
     }
