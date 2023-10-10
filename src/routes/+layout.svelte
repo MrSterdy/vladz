84c8c95b4b9c7cd6480cebe0a7d20514f8ff5eb5
MrSterdy <mrsterdy@gmail.com>
2023-10-10 22:16:24 +0300

    Style main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,39 @@
     import { browser } from "$app/environment";
 
     import "../app.css";
+    import { onMount } from "svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
+
+    let onMainPage = true;
 
     $: if (browser && $page.url.pathname === "/") {
         window.Telegram.WebApp.BackButton.hide();
+        onMainPage = true;
     } else if (browser) {
         window.Telegram.WebApp.BackButton.show();
+        onMainPage = false;
     }
+
+    onMount(() => {
+        document.documentElement.setAttribute(
+            "data-theme",
+            window.Telegram.WebApp.colorScheme
+        );
+    });
 </script>
 
-<section class="prose">
-    <slot />
-</section>
+<main
+    class="flex flex-col h-full gap-4 {onMainPage ? 'bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] to-base-100 from-primary' : ''} "
+>
+    <Navbar transparent={onMainPage} />
+
+    <article class="prose p-4 max-w-full">
+        <slot />
+    </article>
+</main>
+
+<style lang="postcss">
+    :global([data-theme=dark]) article {
+        @apply prose-invert;
+    }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ export default {
     mode: "jit",
     content: ["./src/**/*.{html,js,svelte,ts}"],
     plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/typography")
-    ]
+        require("@tailwindcss/typography"),
+        require("daisyui")
+    ],
+    daisyui: {
+        themes: ["light", "dark"]
+    }
 };
