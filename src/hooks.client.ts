e37ff93bcf8a00e4860cc7738f8e411014f2e4e0
MrSterdy <mrsterdy@gmail.com>
2023-10-11 21:25:48 +0300

    Set color of main button to primary color

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import "dayjs/locale/ru";
 import type { HandleClientError } from "@sveltejs/kit";
+import { hslToHex } from "$lib/utils/color";
 
 export const handleError: HandleClientError = ({ error }) => {
     console.error(error);
@@ -13,5 +14,17 @@ window.Telegram.WebApp.ready();
 window.Telegram.WebApp.BackButton.onClick(() => window.history.back());
 
 window.Telegram.WebApp.expand();
+
+const styles = getComputedStyle(document.body);
+const primaryColor = styles
+    .getPropertyValue("--p")
+    .split(" ")
+    .map(c => parseInt(c));
+
+window.Telegram.WebApp.MainButton.color = hslToHex(
+    primaryColor[0],
+    primaryColor[1],
+    primaryColor[2]
+);
 
 dayjs.locale("ru");
