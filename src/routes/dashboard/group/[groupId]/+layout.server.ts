776b85eada837d103fa440808a0ec1d2130e7c5e
MrSterdy <mrsterdy@gmail.com>
2023-10-11 20:57:59 +0300

    Style main page of group

--- src/routes/dashboard/group/[groupId]/+layout.server.ts
+++ src/routes/dashboard/group/[groupId]/+layout.server.ts
@@ -3,5 +3,5 @@ import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = event => {
     const { group, groupUser } = event.locals;
 
-    return { group: group!, groupUser: groupUser! };
+    return { group: group!, groupUser: groupUser };
 };
--- src/routes/dashboard/group/[groupId]/+page.svelte
+++ src/routes/dashboard/group/[groupId]/+page.svelte
@@ -4,26 +4,65 @@
     import { enhance } from "$app/forms";
     import { goto } from "$app/navigation";
     import MainButton from "$lib/components/MainButton.svelte";
+    import Icon from "$lib/components/Icon.svelte";
+    import { groupUserRoles } from "$lib/consts";
 
     export let data: PageData;
 </script>
 
-<h1>{data.group.name}</h1>
-<h2>Код группы: {data.group.inviteCode}</h2>
+<section class="flex flex-col justify-between h-full gap-5">
+    <div class="card card-compact card-body bg-base-100 grow-0">
+        <h2 class="card-title m-0 gap-3 justify-between">
+            {data.group.name}
+            {#if data.groupUser}
+                <span class="badge badge-accent badge-outline">
+                    {groupUserRoles[data.groupUser.role].toUpperCase()}
+                </span>
+            {/if}
+        </h2>
 
-{#if (data.groupUser && data.groupUser.role !== "APPLICATION") || data.user.role !== "USER"}
-    <a href="composition">Участники</a>
-    <a href="timetable">Расписание</a>
-    <a href="subjects">Предметы</a>
-    <a href="holidays">Выходные</a>
-{/if}
+        <ul class="text-primary flex justify-between gap-3">
+            <li class="flex items-center gap-2">
+                <Icon name="add-user" class="w-5 h-5 fill-base-content" />
+                {data.group.inviteCode}
+            </li>
+            <li class="flex items-center gap-2">
+                <Icon name="users" class="w-5 h-5 fill-base-content" />
+                {data.group.users.length}
+            </li>
+        </ul>
 
-{#if data.groupUser}
-    <form method="post" use:enhance>
-        <input type="submit" value="Покинуть группу" />
-    </form>
-{/if}
+        {#if data.groupUser}
+            <form class="card-actions" method="post" use:enhance>
+                <input
+                    type="submit"
+                    class="btn btn-error w-full"
+                    value="Покинуть группу"
+                />
+            </form>
+        {/if}
+    </div>
 
-{#if data.user.role === "ADMIN" || data.user.role === "HELPER"}
-    <MainButton onClick={() => goto("edit")} text="РЕДАКТИРОВАТЬ" />
-{/if}
+    <div class="flex flex-wrap gap-2">
+        <a
+            role="button"
+            href="subjects"
+            class="btn btn-primary btn-outline grow basis-0">Предметы</a
+        >
+        <a role="button" href="composition" class="btn btn-primary grow basis-0"
+            >Участники</a
+        >
+        <a role="button" href="timetable" class="btn btn-primary grow basis-0"
+            >Расписание</a
+        >
+        <a
+            role="button"
+            href="holidays"
+            class="btn btn-primary btn-outline grow basis-0">Выходные</a
+        >
+
+        {#if data.user.role === "ADMIN" || data.user.role === "HELPER"}
+            <MainButton onClick={() => goto("edit")} text="РЕДАКТИРОВАТЬ" />
+        {/if}
+    </div>
+</section>
