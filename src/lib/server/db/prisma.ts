08962362f97b1cf1661b824ab24988576d275b7c
MrSterdy <mrsterdy@gmail.com>
2023-10-24 22:23:52 +0300

    Debug prisma

--- src/lib/server/db/prisma.ts
+++ src/lib/server/db/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: import.meta.env.DEV ? ["query", "info", "warn", "error"] : ["error"]
+});
 
 export default prisma;
